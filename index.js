1de208675fe2f373d0260aca954460c15467e68e
Eyu-Gen <dreamersthebig@gmail.com>
2024-05-19 20:15:31 +0545

    Completed

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let button1 = document.getElementsByClassName("plusLogo");
 let button2 = document.getElementsByClassName("minusLogo");
-let block = document.getElementsByClassName("block");
+let block = document.getElementsByClassName("hiddenBlock");
 
-console.log(button1);
-console.log(button2);
-console.log(block);
+for (let i = 0; i < (button1.length); i++) {
+    button1[i].addEventListener("click", function() {
+        button1[i].style.display = "none";
+        button2[i].style.display = "block";
+        block[i].style.display = "block";
+    });
+
+    button2[i].addEventListener("click", function() {
+        button2[i].style.display = "none";
+        button1[i].style.display = "block";
+        block[i].style.display = "none";
+    });
+};
